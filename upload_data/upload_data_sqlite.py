c87e108079964b0b3224fa07a7d412046ad55b61
Wesley Steve <wesley.silva23@hotmail.com>
2023-01-07 10:38:25 -0300

    feat: upload_data_sqlite,  added check_db function

--- upload_data/upload_data_sqlite.py
+++ upload_data/upload_data_sqlite.py
@@ -8,3 +8,21 @@ from database.conections.sqlite import create_db_sqlite
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DATA_DIR = os.path.join(BASE_DIR, "data")
+
+def check_db_exists():
+  """
+    Check database file '.db'
+    
+    Returns:
+      False: if not exists
+      True: if exists
+  """
+  
+  
+  base_db = [i for i in os.listdir(DATA_DIR) if i.endswith(".db")]
+  
+  if len(base_db) == 0:
+    return False
+  else:
+    return True
+  
