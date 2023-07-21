943b94be275e1b29516f1419b6591c5cf9daba6c
Wesley Steve <wesley.silva23@hotmail.com>
2023-07-21 14:44:02 -0300

    upload_data_sqlite: added new function create_db_user

--- upload_data/upload_data_sqlite.py
+++ upload_data/upload_data_sqlite.py
@@ -28,6 +28,7 @@ def check_db_exists(data_dir):
     return False
   
 
+
 def create_db_files_xlsx_or_xls(data_dir, file_name=None):
   """
     Create Sqlite database with '.xls' and '.xlsx' files
@@ -78,7 +79,51 @@ def create_db_files_xlsx_or_xls(data_dir, file_name=None):
       
       return True
      
+
     
+def create_db_user(data_dir):
+  """
+    Create Sqlite database with 'tabele user'
+    
+    Returns:
+      Connection
+  """
+  
+  if check_db_exists(data_dir=data_dir):
+    
+    # open connection
+    
+    con = connect_db_sqlite(db_path=data_dir)
+        
+    print("Sqlite database sucessfully connected")
+      
+    return True 
+    
+      
+  else:
+    
+    # open connection
+      
+      con = create_db_sqlite(data_dir, "banco")
+      
+      # if file_name == 'DADOS.xlsx' or file_name == 'DADOS.xls':
+      # df_tmp = pd.read_excel(os.path.join(data_dir, file_name))
+      
+      query = pd.read_sql_query = '''
+                                CREATE TABLE IF NOT EXISTS USERS 
+                                  (
+                                    ID INTEGER PRIMARY KEY AUTOINCREMENT, 
+                                    NAME VARCHAR(100) NOT NULL,
+                                    USER_NAME VARCHAR(50) NOT NULL,
+                                    PASSWORD VARCHAR(64) NOT NULL
+                                  )
+                                ''', con, False
+                                
+      
+      print("Sqlite database sucessfully created")
+      
+      return True
+     
         
     
 
